9b3da7d42a2836ae68ba652385bc32485ba680c9
RiegL <leoorieg@gmail.com>
2024-05-27 21:50:55 -0300

    comentarios / CRUD

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -17,10 +17,12 @@ const clientes = [
   },
 ];
 
+//lista os clientes
 function getClients(req, res) {
   res.send(clientes);
 };
 
+//lista somente o cliente cujo o id foi procurado
 function getOneClient(req, res) {
   let id = req.params.id;
   const cliente = clientes.find((item) => item.id === Number(id));
@@ -31,33 +33,37 @@ function getOneClient(req, res) {
   }
 };
 
+//cria um cliente novo
 createClient = (req, res) => {
   const newClient = req.body;
   if (Object.keys(newClient).length > 0) {
-    clientes.push(newClient);
+   
     res.status(201).send("Cliente Criado");
   } else {
     res.status(406).send("nao adicionou");
   }
 };
 
+//atualiza o cliente pelo id
 function updateCliente(req, res) {
   const id = req.params.id;
-  let indice = findClienteIndex(id);
+  let indice = 
   clientes[indice] = req.body;
   res.status(200).send("Cliente atualizado com sucesso");
 };
 
+//função que procura o index do id
 function findClienteIndex(id) {
   const index = clientes.findIndex((item) => item.id === Number(id));
   return index;
 };
 
+//remove o cliente pelo id selecninado
 removeClient = (req, res) => {
-  const id = req.params.id;
-  let indice = findClienteIndex(id);
-  clientes.splice(indice,1)
-  res.status(200).send(`o Cliente do id ${id} foi removido!`)
+   const id = req.params.id;
+   let indice = findClienteIndex(id);
+   clientes.splice(indice,1)
+   res.status(200).send(`O cliente do id ${id} foi excluido!!`)
 };
 
 module.exports = {
